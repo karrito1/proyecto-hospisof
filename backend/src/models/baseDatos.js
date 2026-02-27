c779946fdc34ee20a509b83199114204ddea1281
Cristian Camilo Peña Villa <kamilovilla554@gmail.com>
2026-02-27 08:50:58 -0500

    creacion de  .env

--- backend/src/models/baseDatos.js
+++ backend/src/models/baseDatos.js
@@ -1,18 +1,23 @@
 // modulo base de datos
 import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config(); // Cargar variables de entorno
 
 let conx;
 
 try {
   conx = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    database: "proyecto_hospisoft",
-    port: "3306",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
     queueLimit: 0,
   });
+  console.log("Conexión a la base de datos establecida correctamente");
 } catch (error) {
   console.error(`Error inesperado: ${error.message}`);
 }
