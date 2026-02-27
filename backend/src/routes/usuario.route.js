237bc06c5deda4adbff347ecdec0ea3d6546e0d6
Cristian Camilo Peña Villa <kamilovilla554@gmail.com>
2026-02-27 10:15:11 -0500

    Arregle la ruta correspondiente

--- backend/src/routes/usuario.route.js
+++ backend/src/routes/usuario.route.js
@@ -4,7 +4,7 @@ import * as usuarioController from "../controllers/usuario.controller.js";
 const router = Router();
 router.post("/usuario/crear", usuarioController.createUsuario);
 router.put("/usuario/editar/:id", usuarioController.updateUsuario);
-router.delete("/usuario/eliminar/:id", usuarioController.deleteUsuario);
+router.put("/usuario/eliminar/:id", usuarioController.deleteUsuario);
 router.get("/usuario/listarTodos", usuarioController.getUsuarios);
 router.get("/usuario/listarporid/:id", usuarioController.getUsuarioById);
 
